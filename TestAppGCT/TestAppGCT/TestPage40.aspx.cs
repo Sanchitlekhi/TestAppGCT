062e103f301cc9b5e293b7a65285b8a7e5df84e4
Lavkesh <lavkesh@graycelltech.com>
2016-12-16 18:58:16 +0530

    Test

---
+++ TestAppGCT/TestAppGCT/TestPage40.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestAppGCT
+{
+    public partial class TestPage40 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Current Date Time="+DateTime.Now.ToString());     
+        }
+    }
+}
